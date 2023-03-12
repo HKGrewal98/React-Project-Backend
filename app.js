71eb837e70844bde8e1486450d7c60f704c2b1ee
HKGrewal98 <104564558+HKGrewal98@users.noreply.github.com>
2023-03-12 14:36:56 -0400

    Final Build

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 const reviewsRoutes = require('./routes/reviewsRoutes')
 const contactRoutes = require('./routes/contactRoutes');
-
+const path = require('path')
 
 async function serverUp(){
     const state = await databaseConnection()
@@ -16,17 +16,22 @@ async function serverUp(){
        process.exit(0)
     }
     // Parse request bodies as JSON
-    app.use(express.json());
+    app.use(express.static(path.join(__dirname, "./client/build")))
     app.use('/menu',menuRoutes)
     app.use('/web',webTokenRoute)
     app.use('/user',userRoutes)
     app.use('/order',orderRoutes)
     app.use('/reviews',reviewsRoutes)
     app.use('/contact', contactRoutes);
+    app.get("/*", function (req, res) {
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+      })
+    
     app.listen(8080,()=>{
     console.log("Server up and listening at 8080.")
     })
 }
+
 
 
 serverUp()
